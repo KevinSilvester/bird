5a1bbab4f40a0f4daf82161a8a80e6342050c593
Kevin Silvester <kevinsilvester05@gmail.com>
2022-06-12 02:01:52 +0100

    removed unused function and imports in files

--- src/utils/files.rs
+++ src/utils/files.rs
@@ -1,11 +1,6 @@
-use crate::core::{BirdConfig, EggItem, Eggs, Nest, NestItem};
 use crate::utils::errors::BirdError;
-use serde_json;
-use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
 
 pub fn create_file(path: &str) -> Result<(), BirdError> {
    File::create(path)?;
@@ -25,37 +20,3 @@ pub fn read_file(path: &str) -> Result<String, BirdError> {
    Ok(contents)
 }
 
-pub fn eggs_to_hashmap(config: &BirdConfig) -> Result<HashMap<String, EggItem>, BirdError> {
-   let json = read_file(&config.eggs_file)?;
-
-   let parsed_json: Eggs = match serde_json::from_str(&json) {
-      Ok(s) => s,
-      Err(err) => return Err(BirdError::JsonError((".bird-egg.json".to_owned(), err.to_string()))),
-   };
-
-   Ok(parsed_json.eggs)
-}
-
-// pub fn nest_to_hashmap(config: &BirdConfig) -> Result<HashMap<String, NestItem>, BirdError> {
-//    // check if nest file already exists
-//    match Path::new(&config.nest_file).exists() {
-//       true => {
-// let json = read_file(&config.nest_file)?;
-
-//          let parsed_json: Nest = match serde_json::from_str(&json) {
-//             Ok(s) => s,
-//             Err(err) => {
-//                return Err(BirdError::JsonError((
-//                   ".bird-nest.json".to_owned(),
-//                   err.to_string(),
-//                )))
-//             }
-//          };
-
-//          return Ok(parsed_json.nest);
-//       }
-//       false => {
-//
-//       }
-//    }
-// }
