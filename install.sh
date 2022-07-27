b01bcb01b189cb67c4dc024d3e59a2da839361d0
Kevin Silvester <kevinsilvester05@gmail.com>
2022-07-28 00:27:20 +0100

    fix: install command fail fixed

--- install.sh
+++ install.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
 
-USR_DIR=$(pwd)
-TMP_DIR="/tmp/bird"
+function install_bird () {
+   USR_DIR=$(pwd)
+   TMP_DIR="/tmp/bird"
 
-## RETRIEVE URL TO PROGRAM TARBALL
-echo -e "\nRetrieving download URL..."
-TAR_URL=$(curl -s https://api.github.com/repos/KevinSilvester/bird/releases/latest | grep "browser_download_url.*x86_64-unknown-linux-musl.tar.gz" | cut -d : -f 2,3 | tr -d \")
-TAR_FILE=$(echo $TAR_URL | awk -F/ '{print $NF}')
+   ## RETRIEVE URL TO PROGRAM TARBALL
+   echo -e "\nRetrieving download URL..."
+   TAR_URL=$(curl -s https://api.github.com/repos/KevinSilvester/bird/releases/latest | grep "browser_download_url.*x86_64-unknown-linux-musl.tar.gz" | cut -d : -f 2,3 | tr -d \")
+   TAR_FILE=$(echo $TAR_URL | awk -F/ '{print $NF}')
 
-if [ -z $TAR_URL ]; then
-   echo -e "\nFailed retrieving download URL! ☹️"
-   return 1
-fi
+   if [ -z $TAR_URL ]; then
+      echo -e "\nFailed retrieving download URL! ☹️"
+      return 1
+   fi
 
-echo -e "URL retrieved!\n"
-
-
-## DOWNLAOD THE TARBALL
-echo -e "\nDownloading file"
-[ -d $TMP_DIR ] && rm -rf $TMP_DIR
-mkdir -p $TMP_DIR && cd $TMP_DIR
-
-wget $TAR_URL
-tar -xf $TAR_FILE
-
-if [ -f "bird" ]; then
-   echo -e "\nFailed extracting binary! ☹️"
-   return 1
-fi
+   echo -e "URL retrieved!\n"
 
 
-## INSTALL PROGRAM
-echo -e "\nInstalling bird"
-[ -d "$HOME/.local/bin" ] && mkdir - "$HOME/.local/bin"
-[ -f "$HOME/.loca/bin/bird"] && rm "$HOME/.loca/bin/bird"
-mv bird ~/.local/bin
+   ## DOWNLAOD THE TARBALL
+   echo -e "\nDownloading file"
+   [ -d $TMP_DIR ] && rm -rf $TMP_DIR
+   mkdir -p $TMP_DIR && cd $TMP_DIR
+
+   wget $TAR_URL
+   tar -xf $TAR_FILE
+
+   if [ ! -f "bird" ]; then
+      echo -e "\nFailed extracting binary! ☹️"
+      return 1
+   fi
 
 
-## FINISH INSTALL
-if [[ ! ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
-  echo -e "\nAdd '~/.local/bin' to you PATH to use bird"
-fi
+   ## INSTALL PROGRAM
+   echo -e "\nInstalling bird"
+   [ -d "$HOME/.local/bin" ] && mkdir - "$HOME/.local/bin"
+   [ -f "$HOME/.loca/bin/bird"] && rm "$HOME/.loca/bin/bird"
+   mv bird ~/.local/bin
 
-cd $USR_DIR
 
-echo -e "\nbird install complete! 😄"
+   ## FINISH INSTALL
+   if [[ ! ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
+     echo -e "\nAdd '~/.local/bin' to you PATH to use bird"
+   fi
+
+   cd $USR_DIR
+}
+
+install_bird /
+   && echo -e "\nbird install complete! 😄"
+   || echo -e "\nbird install failed! ☹️"
